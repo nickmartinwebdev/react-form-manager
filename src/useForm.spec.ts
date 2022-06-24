5d2c28db3545444fbe0e8de8ab37d0d4b2856d69
Nicholas Martin <nickmartinwebdev@gmail.com>
2022-06-24 19:28:01 +1000

    all state values working unnested

--- src/useForm.spec.ts
+++ src/useForm.spec.ts
@@ -3,27 +3,46 @@ import { it, expect } from "vitest";
 
 import { useForm } from "./useFormNew";
 
-it("should return initial values as values", () => {
-
+it("should return field values from initial values", () => {
   const initialValues = {
-    name: [{
-      first: 'hey',
-      last: ''
-    }]
-  }
+    object: {
+      property1: "property 1",
+      objectproperty: {
+        property2: "property 2",
+      },
+    },
+    string: "string",
+    number: 10,
+    boolean: true,
+    objectArray: [{ property3: "property 3" }],
+    primitiveArray: ["string"],
+    emptyArray: [] as string[],
+  };
 
-  const { result } = renderHook(() => useForm({
-    initialValues,
-    conditionalValues: {
-      name: {
-        conditional: {
-          pleaseWork: (values) => values.name[0].first === ''
-        }
-      }
-    }
-  }))
+  const { result } = renderHook(() =>
+    useForm({
+      initialValues,
+    })
+  );
 
-  console.log(result.current.fields.name)
+  console.log(result.current.fields);
 
-  expect(result.current.fields.name.value).toMatchObject([{ first: '', last: '' }])
+  expect(result.current.fields.string.value).toBe("string");
+  expect(result.current.fields.number.value).toBe(10);
+  expect(result.current.fields.boolean.value).toBe(true);
+  expect(result.current.fields.object.value).toMatchObject({
+    property1: "property 1",
+    objectproperty: {
+      property2: "property 2",
+    },
+  });
+  expect(result.current.fields.objectArray.value).toMatchObject([
+    { property3: "property 3" },
+  ]);
+  expect(result.current.fields.primitiveArray.value).toMatchObject(["string"]);
+  expect(result.current.fields.emptyArray.value).toMatchObject([]);
+
+  // expect(result.current.fields.object.fields.property1.value).toBe(
+  //   "property 1"
+  // );
 });
--- src/useFormNew.ts
+++ src/useFormNew.ts
@@ -1,86 +1,222 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import { ConditionalValues, SubmitFuncMap, ValidationValues, FormData, ConditionalValueResults, DropEmpty } from "./types"
+import {
+  ConditionalValues,
+  SubmitFuncMap,
+  ValidationValues,
+  FormData,
+  ConditionalValueResults,
+  DropEmpty,
+} from "./types";
 
-interface Props<T, R extends ConditionalValues<T, T>, S extends SubmitFuncMap<T>> {
-    initialValues: T,
-    conditionalValues?: R,
-    validation?: ValidationValues<T>,
-    submit?: S
+interface Props<
+  T,
+  R extends ConditionalValues<T, T>,
+  S extends SubmitFuncMap<T>
+> {
+  initialValues: T;
+  conditionalValues?: R;
+  validation?: ValidationValues<T>;
+  submit?: S;
 }
 
+const evaluateConditionalValues = <T, U, C extends ConditionalValues<T, U>>(
+  key: keyof T,
+  state: U,
+  conditionalMap: C
+): ConditionalValueResults<T, U, C> => {
+  const conditionalValues: Partial<ConditionalValueResults<T, U, C>> = {};
+  if (conditionalMap[key]) {
+    Object.entries(conditionalMap[key]["conditional"]).forEach(
+      ([cKey, cValue]) => {
+        const typedCKey = cKey as keyof C[keyof T]["conditional"];
+        const typedCValue =
+          cValue as C[keyof T]["conditional"][typeof typedCKey];
+        const cResult = typedCValue(state);
+        conditionalValues[typedCKey] = cResult;
+      }
+    );
+  }
 
-const createFormData = <T, R extends ConditionalValues<T, T>, S extends SubmitFuncMap<T>>(state: T, conditionalMap: R): FormData<T, R, S> => {
+  return conditionalValues as ConditionalValueResults<T, U, C>;
+};
 
-    const formData: Partial<FormData<T, R, S>> = {}
+const createFormData = <
+  T,
+  R extends ConditionalValues<T, T>,
+  S extends SubmitFuncMap<T>
+>(
+  state: T,
+  conditionalMap: R
+): FormData<T, R, S> => {
+  const formData: Partial<FormData<T, R, S>> = {};
 
-    Object.entries(state).forEach(([key, value]) => {
-        const typedKey = key as keyof T;
-        const typedValue = value as T[keyof T]
+  Object.entries(state).forEach(([key, value]) => {
+    const typedKey = key as keyof T;
+    const typedValue = value as T[keyof T];
 
-        // If value is an array
-        if (Array.isArray(typedValue)) {
-            if (typeof typedValue === 'object') {
-                const conditionalValues: Partial<ConditionalValueResults<
-                    T[keyof T],
-                    T,
-                    ConditionalValues<T[keyof T], T>>> = {}
+    // If value is an array
+    if (Array.isArray(typedValue)) {
+      if (typedValue.length) {
+        if (typedValue[0] === Object(typedValue[0])) {
+          const conditionalValues = evaluateConditionalValues(
+            typedKey,
+            state,
+            conditionalMap
+          );
+          // Need to drop properties conditional values and submit if required
+          const possiblyEmpty = {
+            conditionalValues: conditionalValues as ConditionalValueResults<
+              T[keyof T],
+              T,
+              ConditionalValues<T[keyof T], T>
+            >,
+          } as DropEmpty<{
+            conditionalValues: ConditionalValueResults<
+              T[keyof T],
+              T,
+              ConditionalValues<T[keyof T], T>
+            >;
+          }>;
 
-                if (conditionalMap[typedKey]) {
-                    Object.entries(conditionalMap[typedKey]['conditional']).forEach(([cKey, cValue]) => {
-                        const typedCKey = cKey as keyof R[keyof T]['conditional']
-                        const typedCValue = cValue as R[keyof T]['conditional'][typeof typedCKey]
-                        const cResult = typedCValue(state)
-                        conditionalValues[typedCKey] = cResult
-                    })
-                }
+          formData[typedKey] = {
+            value: state[typedKey],
+            error: null,
+            ...possiblyEmpty,
+            items: [],
+          } as FormData<T, R, S>[keyof T];
+        } else {
+          const conditionalValues = evaluateConditionalValues(
+            typedKey,
+            state,
+            conditionalMap
+          );
 
-                // Need to drop properties conditional values and submit if required
-                const possiblyEmpty = {
-                    conditionalValues: conditionalValues as ConditionalValueResults<
-                        T[keyof T],
-                        T,
-                        ConditionalValues<T[keyof T], T>>
-                } as DropEmpty<{
-                    conditionalValues: ConditionalValueResults<
-                        T[keyof T],
-                        T,
-                        ConditionalValues<T[keyof T], T>>
-                }>
+          // Need to drop properties conditional values and submit if required
+          const possiblyEmpty = {
+            conditionalValues: conditionalValues as ConditionalValueResults<
+              T[keyof T],
+              T,
+              ConditionalValues<T[keyof T], T>
+            >,
+          } as DropEmpty<{
+            conditionalValues: ConditionalValueResults<
+              T[keyof T],
+              T,
+              ConditionalValues<T[keyof T], T>
+            >;
+          }>;
 
-                formData[typedKey] = {
-                    value: state[typedKey],
-                    error: null,
-                    ...possiblyEmpty,
-                    items: [],
-                } as FormData<T, R, S>[keyof T]
-            }
+          formData[typedKey] = {
+            value: state[typedKey],
+            error: null,
+            ...possiblyEmpty,
+            items: [],
+          } as FormData<T, R, S>[keyof T];
         }
-    })
+      } else {
+        const conditionalValues = evaluateConditionalValues(
+          typedKey,
+          state,
+          conditionalMap
+        );
 
-    return formData as FormData<T, R, S>
+        // Need to drop properties conditional values and submit if required
+        const possiblyEmpty = {
+          conditionalValues: conditionalValues as ConditionalValueResults<
+            T[keyof T],
+            T,
+            ConditionalValues<T[keyof T], T>
+          >,
+        } as DropEmpty<{
+          conditionalValues: ConditionalValueResults<
+            T[keyof T],
+            T,
+            ConditionalValues<T[keyof T], T>
+          >;
+        }>;
 
-}
+        formData[typedKey] = {
+          value: state[typedKey],
+          error: null,
+          ...possiblyEmpty,
+          items: [],
+        } as FormData<T, R, S>[keyof T];
+      }
+    } else if (typeof typedValue === "object") {
+      const conditionalValues = evaluateConditionalValues(
+        typedKey,
+        state,
+        conditionalMap
+      );
+
+      // Need to drop properties conditional values and submit if required
+      const possiblyEmpty = {
+        conditionalValues: conditionalValues as ConditionalValueResults<
+          T[keyof T],
+          T,
+          ConditionalValues<T[keyof T], T>
+        >,
+      } as DropEmpty<{
+        conditionalValues: ConditionalValueResults<
+          T[keyof T],
+          T,
+          ConditionalValues<T[keyof T], T>
+        >;
+      }>;
+
+      formData[typedKey] = {
+        value: state[typedKey],
+        error: null,
+        ...possiblyEmpty,
+      } as FormData<T, R, S>[keyof T];
+    } else {
+      const conditionalValues = evaluateConditionalValues(
+        typedKey,
+        state,
+        conditionalMap
+      );
+
+      // Need to drop properties conditional values and submit if required
+      const possiblyEmpty = {
+        conditionalValues: conditionalValues as ConditionalValueResults<
+          T[keyof T],
+          T,
+          ConditionalValues<T[keyof T], T>
+        >,
+      } as DropEmpty<{
+        conditionalValues: ConditionalValueResults<
+          T[keyof T],
+          T,
+          ConditionalValues<T[keyof T], T>
+        >;
+      }>;
+      formData[typedKey] = {
+        value: state[typedKey],
+        error: null,
+        ...possiblyEmpty,
+      } as FormData<T, R, S>[keyof T];
+    }
+  });
+
+  return formData as FormData<T, R, S>;
+};
 
 export const useForm = <
-    T,
-    R extends ConditionalValues<T, T>,
-    S extends SubmitFuncMap<T>>(props: Props<T, R, S>) => {
+  T,
+  R extends ConditionalValues<T, T>,
+  S extends SubmitFuncMap<T>
+>(
+  props: Props<T, R, S>
+) => {
+  const { initialValues, conditionalValues = {}, validation, submit } = props;
 
-    const { initialValues, conditionalValues = {}, validation, submit } = props
+  const [state, setState] = useState<T>(initialValues);
+  const [errorMap, setErrorMap] = useState<{}>({});
 
-    const [state, setState] = useState<T>(initialValues)
-    const [errorMap, setErrorMap] = useState<{}>({})
+  const form: FormData<T, R, S> = createFormData(state, conditionalValues);
 
-    const form: FormData<T, R, S> = createFormData(state, conditionalValues)
-
-    return {
-        fields: form
-    }
-}
-
-
-
-
-
-
+  return {
+    fields: form,
+  };
+};
