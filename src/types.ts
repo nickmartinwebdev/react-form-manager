bcd67e017dbf0dd6bee1a5378f94c66d1983f69c
Nick Martin <nickm@xyris.com.au>
2022-06-24 14:33:40 +1000

    submit type working

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { updateItemAtIndex } from "./utils/array";
-
 
 type ActiveFunc<T> = (values: T) => boolean;
 
@@ -25,60 +23,78 @@ type ValidationValues<T extends Record<string, any>> = { [Key in keyof T]?:
   } & { fields?: ValidationValues<T[Key]> } :
   { validator?: ValidatorFunc<T[Key]> } }
 
-function createStyleMap<S extends Record<string, any>, T extends ConditionalValues<S, S>>(_: S, cfg: T) {
-  return cfg;
-}
-
 type ConditionalValueResults<T, U, C extends ConditionalValues<T, U>> = {
   [Key in keyof C]: boolean
 }
 
-type DropEmpty<T> =
-  { [K in keyof T as keyof T[K] extends never ? never : K]: T[K] };
+type SubmitFunc<TValue> = (value : TValue) =>  Promise<void>
 
-type DData<T1, T2, C extends ConditionalValues<T1, T2>> = {
-  value: T1;
-  error: string | null;
-  conditionalValues: C[keyof T1]['conditional'] extends Record<string, any> ? ConditionalValueResults<T1, T2, C[keyof T1]['conditional']> : never
+type SubmitFuncMap<TState> = {[Key in keyof TState] ?: TState[Key] extends Array<infer I> ? 
+  I extends Record<string, any> ? {
+    submit ?: SubmitFunc<TState[Key]>
+  } & {fields? : SubmitFuncMap<I>}: {
+    submit ?: SubmitFunc<TState[Key]>
+  } :
+  TState[Key] extends Record<string, any> ? {
+  submit ?: SubmitFunc<TState[Key]>
+} & {fields?: SubmitFuncMap<TState[Key]>} : {submit?: SubmitFunc<TState[Key]>}}
+
+type DropEmpty<T> =
+  { [K in keyof T as keyof T[K] extends Function ? K : T[K] extends never ? never : K  ]: T[K] };
+
+
+
+type NonObjectArrayData<T> = {
+  value: T;
 }
 
-type FormData<T, C extends ConditionalValues<T, T>> = {
+type FormData<T, C extends ConditionalValues<T, T>, S extends SubmitFuncMap<T>> = {
   [Key in keyof T]: T[Key] extends Array<infer I> ?
+  I extends Record<string, any> ?
   DropEmpty<{
     value: T[Key];
     error: string | null;
+    submit: S[Key] extends {submit: Function } ? () => Promise<SubmitFunc<T[Key]>> : never;
     conditionalValues: C[Key]['conditional'] extends Record<string, any> ?
     ConditionalValueResults<T, T, C[Key]['conditional']> : never
-  }> & { fields: FormData<I, C[Key] extends Record<string, any> ? C[Key]['fields'] : never>[] }
+  }> & { items: { fields: FormData<I, C[Key] extends Record<string, any> ? C[Key]['fields'] : never, S[Key] extends Record<'fields' | 'submit', any> ? S[Key]['fields']: never> }[] } :
+  DropEmpty<{
+    value: T[Key];
+    error: string | null;
+    submit: S[Key] extends {submit: Function } ? () => Promise<SubmitFunc<T[Key]>> : never;
+    conditionalValues: C[Key]['conditional'] extends Record<string, any> ?
+    ConditionalValueResults<T, T, C[Key]['conditional']> : never
+  }> & { items: NonObjectArrayData<I>[] }
   : T[Key] extends Record<string, any> ?
   DropEmpty<{
     value: T[Key];
     error: string | null;
+    submit: S[Key] extends {submit: Function } ? () => Promise<SubmitFunc<T[Key]>> : never;
     conditionalValues: C[Key]['conditional'] extends Record<string, any> ?
     ConditionalValueResults<T, T, C[Key]['conditional']> : never
-  }> & { fields: FormData<T[Key], C[Key] extends Record<string, any> ? C[Key]['fields'] : never> }
-
+  }> & { fields: FormData<T[Key], C[Key] extends Record<string, any> ? C[Key]['fields'] : never, S[Key] extends Record<'fields' | 'submit', any> ? S[Key]['fields']: never>  }
   : DropEmpty<{
     value: T[Key];
     error: string | null;
+    submit: S[Key] extends {submit: Function } ? () => Promise<SubmitFunc<T[Key]>> : never;
     conditionalValues: C[Key]['conditional'] extends Record<string, any> ?
     ConditionalValueResults<T, T, C[Key]['conditional']> : never
   }>
 }
 
-const useForm = <T, R extends ConditionalValues<T, T>>(props: { initialValues: T, conditionalValues?: R, validation?: ValidationValues<T> }) => {
-  const { initialValues, conditionalValues } = props
-  const types = createStyleMap(initialValues, conditionalValues)
-  return { values: initialValues, conditions: types }
+export const useForm = <T, R extends ConditionalValues<T, T>, S extends SubmitFuncMap<T>>(props: { initialValues: T, conditionalValues?: R, validation?: ValidationValues<T>, submit?: S }) => {
+  const { initialValues, conditionalValues, submit } = props
+  return { values: initialValues, conditions: conditionalValues, submit }
 }
 
 const at = useForm({
   initialValues: {
-    name: {
+    hey: '',
+        name: {
       first: '',
       last: ''
     },
-    values: { please: '', work: 4 }
+    values: [{text: ''}]
   },
   conditionalValues: {
     name: {
@@ -92,6 +108,23 @@ const at = useForm({
           }
         }
       }
+    },
+    values: {
+      conditional:{
+        
+      },
+      fields: {
+        text: {
+          conditional: {
+            tyr: () => true
+          }
+        }
+      }
+    },
+    hey: {
+      conditional: {
+        hi : () => true
+      }
     }
   },
   validation: {
@@ -99,12 +132,28 @@ const at = useForm({
       validator: (val) => '',
     },
     values: {
+      validator: () => 'true'
     }
+  },
+  submit: {
+    name: {
+      submit: async (value) => {}
+    },
+    values: {
+      submit: async (hey) => {},
+      fields: {
+        text: {
+
+        }
+      }
+    },
+
+
   }
 })
 
-const v: FormData<typeof at['values'], typeof at['conditions']> = {}
+const v: FormData<typeof at['values'], typeof at['conditions'], typeof at['submit']> = {}
 
-v.values.fields.please.value
+v.hey.
 
 
